8f36805628b59aca5458047ffeacfd8e094275f6
Michael Kostaman <kostamanmichael@gmail.com>
2024-05-21 11:20:16 +0700

    Initial Commit

---
+++ HealthSnap/HealthSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthSnapApp.swift
+//  HealthSnap
+//
+//  Created by Michael Varian Kostaman on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
